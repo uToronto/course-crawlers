8e77026d6d393ebc4b2afcd7c5bea8320d80e980
William Mak <william.mak@hotmail.ca>
2015-05-02 18:25:49 -0400

    adding scrapy initial files

---
+++ utm/utm/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for utm project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'utm'
+
+SPIDER_MODULES = ['utm.spiders']
+NEWSPIDER_MODULE = 'utm.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'utm (+http://www.yourdomain.com)'
